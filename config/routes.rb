1d29faa40152a947bd6804ded3250dcad58db0f7
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-11-21 09:52:18 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # root "recipes#public_recipes"
-  get "users/shopping_list"
-
-  resources :foods, only: [:index, :new, :create, :destroy]
+  devise_for :users
+   root "foods#index"
+get "users/general_shopping_list"
+  resources :foods, except: :update
+  resources :recipes, except: :update do
+    member do
+      put :toggle_public
+    end
+    resources :recipe_foods, only: [:index, :new, :create, :destroy]
+  end
+  get '/public_recipes', to: 'public_recipes#index', as: 'public_recipes'
+end
