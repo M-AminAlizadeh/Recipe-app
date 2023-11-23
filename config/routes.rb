603762647e986f7067bc7f695b61fbc96613aee7
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-11-23 11:41:42 +0330

    Add log-out link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # get 'recipes/index'
+  get '/sign_out_user', to: 'users#sign_out_user', as: 'sign_out_user'
   devise_for :users
-
   devise_scope :user do
     authenticated :user do
       root to: 'home#index', as: :authenticated_root
