048cc7134dba962e2cba49adbf8996e78f9e8a22
M-AminAlizadeh <mohammadaminalizadeh1378@gmail.com>
2023-11-25 08:23:17 +0330

    Add foods to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,25 @@
   )
 
   3.times do |j|
-    user.recipes.create!(
+    recipe = user.recipes.create!(
       title: "Recipe #{j + 1} for User #{i + 1}",
       description: "This is recipe #{j + 1} for user #{i + 1}.",
       preparation_time: rand(1..3),
       cooking_time: rand(20..60),
       public: [true, false].sample
     )
+
+    4.times do |k|
+      food = Food.create!(
+        name: "Food #{k + 1} for User #{i + 1} Recipe #{j + 1}",
+        measurement_unit: ['Unit', 'Kg', 'Liter'].sample,
+        unit_price: rand(1.0..10.0).round(2)
+      )
+
+      recipe.recipe_foods.create!(
+        food: food,
+        quantity: rand(1..5)
+      )
+    end
   end
 end
